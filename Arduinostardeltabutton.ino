e345f4a1058df26144d4d3476885914541b98f91
novprasetio-sys <novprasetio@gmail.com>
2025-12-14 12:06:37 +0700

    Update Arduinostardeltabutton.ino

--- Arduinostardeltabutton.ino
+++ Arduinostardeltabutton.ino
@@ -1,29 +1,29 @@
-const int onButton = 11;
+const int onButton = 13;
 const int offButton = 12;
-const int overloadButton = 13;
-const int runLed = 5;
-const int starLed = 6;
-const int deltaLed = 7;
+const int overloadButton = 11;
+const int runLed = 2;
+const int starLed = 3;
+const int deltaLed = 4;
 unsigned long previousMillis = 0;
 const long interval = 5000; // 5 detik
 int motorState = 0; // 0: OFF, 1: STAR, 2: DELTA
 void setup() {
-pinMode(onButton, INPUT);
-pinMode(offButton, INPUT);
-pinMode(overloadButton, INPUT);
+pinMode(onButton, INPUT_PULLUP);
+pinMode(offButton, INPUT_PULLUP);
+pinMode(overloadButton, INPUT_PULLUP);
 pinMode(runLed, OUTPUT);
 pinMode(starLed, OUTPUT);
 pinMode(deltaLed, OUTPUT);
 }
 void loop() {
-if (digitalRead(offButton) == HIGH || digitalRead(overloadButton) == HIGH) 
+if (digitalRead(offButton) == LOW || digitalRead(overloadButton) == LOW) 
 {
 motorState = 0;
 digitalWrite(runLed, LOW);
 digitalWrite(starLed, LOW);
 digitalWrite(deltaLed, LOW);
 }
-if (digitalRead(onButton) == HIGH && motorState == 0) 
+if (digitalRead(onButton) == LOW && motorState == 0) 
 {
 motorState = 1;
 digitalWrite(runLed, HIGH);
